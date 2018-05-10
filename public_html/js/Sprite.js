fa253ea0c13e16a0446347baee107b750e7470d2
Unknown <eugeniopb4@gmail.com>
2018-05-10 19:02:59 -0300

    Site

---
+++ public_html/js/Sprite.js
@@ -0,0 +1,166 @@
+
+function Sprite(img) {
+    //Variaveis
+    this.srcX = this.srcY = 0;
+    this.width = 119;
+    this.height = 51;
+    this.speed = 0.5;
+    this.posX = 570;
+    this.posY = 0;
+    this.img = img;
+    this.aux = false;
+    this.mvRight = this.mvLeft = this.mvUp = this.mvDown = false;
+    //Metodos
+
+    //Desenha
+    this.draw = function(ctx) {
+        ctx.drawImage(this.img, this.srcX, this.srcY, this.width, this.height, this.posX, this.posY, this.width, this.height);
+        //this.animacao();
+    };
+
+    //Mover
+    this.mvBiblioteca = function() {
+        if (this.posY <= 60) {
+            this.posY += 1;
+        }
+        if (this.posY == 61) {
+            if (this.posX >= 495) {
+                this.posX -= 1;
+            }
+        }
+        if (this.posX == 494) {
+            if (this.posY <= 90) {
+                this.posY += 1;
+            } else if (this.posY < 300) {
+                this.aux = true;
+            }
+        }
+
+
+        if (this.aux) {
+            this.posY += 0.5;
+            this.posX -= 1;
+            if (this.posX == 360) {
+                this.aux = false;
+            }
+        }
+        if (this.posY >= 158 && this.posY <= 476) {
+            this.posY += 1;
+            if (this.posY == 476) {
+                this.srcY = this.height;
+            }
+        }
+        if (this.posY == 477 && this.posX <= 600) {
+            this.posX += 1;
+
+            if (this.posY == 477 && this.posX == 600)
+            {
+                this.posX = 570;
+                this.posY = 0;
+                this.srcY = this.height - 51;
+            }
+
+        }
+
+    };
+    this.mvProUniFiesRH = function() {
+        if (this.posY <= 60) {
+            this.posY += 1;
+        }
+        if (this.posY == 61) {
+            if (this.posX >= 495) {
+                this.posX -= 1;
+            }
+        }
+        if (this.posX == 494) {
+            if (this.posY <= 90) {
+                this.posY += 1;
+            }
+        }
+        if (this.posY == 91) {
+            if (this.posX <= 576) {
+                this.posX += 1;
+                this.srcY = this.height;//virar personagem para direita
+            }
+        }
+
+    };
+    this.mvCantina = function() {
+        if (this.posY <= 60) {
+            this.posY += 1;
+        }
+        if (this.posY == 61) {
+            if (this.posX >= 495) {
+                this.posX -= 1;
+            }
+        }
+        if (this.posX == 494) {
+            if (this.posY <= 90) {
+                this.posY += 1;
+            }
+        }
+        if (this.posY == 91) {
+            if (this.posX >= 474) {
+                this.posX -= 1;
+            }
+        }
+        if (this.posX == 473) {
+            if (this.posY <= 128) {
+                this.posY += 1;
+            }
+        }
+        if (this.posY == 129) {
+            if (this.posX >= 452) {
+                this.posX -= 1;
+            }
+        }
+        if (this.posX == 451) {
+            if (this.posY <= 196) {
+                this.posY += 1;
+                this.srcY = this.height;//virar personagem para direita
+            }
+        }
+        if (this.posY == 197) {
+            if (this.posX <= 577) {
+                this.posX += 1;
+            }
+        }
+    };
+
+
+    this.move = function() {
+
+        if (this.mvRight) {
+            this.posX += this.speed;
+            this.srcY = this.height;
+        } else
+        if (this.mvLeft) {
+            this.posX -= this.speed;
+            this.srcY = this.height - 51;
+        } else
+        if (this.mvUp) {
+            this.posY -= this.speed;
+        } else
+        if (this.mvDown) {
+            this.posY += this.speed;
+        }
+
+    };
+
+
+    this.animacao = function() {
+        if (this.mvLeft || this.mvUp || this.mvRight || this.mvDown) {
+            //Caso qualquer seta seja pressionada, o contador de animação é incrementado
+            this.countAnim++;
+            if (this.countAnim >= 31) {
+                this.countAnim = 0;
+            }
+            this.srcX = Math.floor(this.countAnim / 1) * this.width;
+        } else {
+            //Caso nenhuma tecla seja pressionada, o contador de animação é zerado e a imagem do personagem parado é exibida
+            this.srcX = 0;
+            this.countAnim = 0;
+        }
+    };
+}
+
---
+++ public_html/js/funcao.js
@@ -0,0 +1,135 @@
+window.onload = function() {
+
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
+    //
+    var left = 37, up = 38, right = 39, down = 40;
+    var boy = new Image();
+    boy.src = "img/Boy.png";
+    var boy = new Sprite(boy);
+
+
+
+    var div = document.getElementById("textDiv");
+    var div2 = document.getElementById("textDiv2");
+    var local = document.getElementById("input")
+    boy.onload = function() {
+
+        init();
+    };
+    function init() {
+        loop();
+    }
+    var srcX = -4;
+    var srcY = 0;
+    var size = 635;
+    var DescerFunc;
+    DescerFunc = setInterval(Descer, 15);
+    var map = new Image();
+    map.src = "img/mapa.jpg";
+    map.onload = function() {
+        loop();
+    };
+
+
+
+    window.addEventListener("keydown", keydownHandler, false);
+    window.addEventListener("keyup", keyupHandler, false);
+
+    function keydownHandler(e) {
+        switch (e.keyCode) {
+            case right:
+                boy.mvRight = true;
+                boy.mvLeft = false;
+                boy.mvUp = false;
+                boy.mvDown = false;
+                break;
+            case left:
+                boy.mvRight = false;
+                boy.mvLeft = true;
+                boy.mvUp = false;
+                boy.mvDown = false;
+                break;
+            case up:
+                boy.mvRight = false;
+                boy.mvLeft = false;
+                boy.mvUp = true;
+                boy.mvDown = false;
+                break;
+            case down:
+                boy.mvRight = false;
+                boy.mvLeft = false;
+                boy.mvUp = false;
+                boy.mvDown = true;
+                break;
+        }
+    }
+    function keyupHandler(e) {
+        switch (e.keyCode) {
+            case right:
+                boy.mvRight = false;
+                break;
+            case left:
+                boy.mvLeft = false;
+                break;
+            case up:
+                boy.mvUp = false;
+                break;
+            case down:
+                boy.mvDown = false;
+                break;
+        }
+    }
+    var mover = false;
+    function verificarLocal() {
+        document.getElementById("submit").onclick = function(e) {
+            switch (local.value) {
+                case "cantina":
+                    mover = true;
+
+                    break;
+                case "dti":
+                    alert("dti");
+                    break;
+            }
+            e.preventDefault();
+        };
+    }
+
+    function moverLocal(mv) {
+        if (mv) {
+            boy.mvCantina();
+        }
+    }
+    function paraTime() {
+        if (srcY >= 672) {
+            clearInterval(DescerFunc);
+        }
+    }
+    function Descer() {
+        //srcY += 1;
+    }
+
+    function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(map, srcX, srcY, size, size, 0, 0, canvas.width, canvas.height);
+        boy.draw(ctx);
+    }
+
+    function update() {
+        boy.move();
+    }
+    function loop() {
+        requestAnimationFrame(loop, canvas);
+        render();
+        update();
+        paraTime();
+        div.textContent = boy.posX;
+        div2.textContent = boy.posY;
+        verificarLocal();
+        moverLocal(mover);
+    }
+
+
+};
+
